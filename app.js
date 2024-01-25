8ad72db260c5bc488228af8f361604acb6c49d25
Jakob <norman9095@gmail.com>
2024-01-25 15:37:25 +0100

    Added security features, cleaned up code

--- app.js
+++ app.js
@@ -4,16 +4,31 @@ const mongoose = require('mongoose');
 const propertyRoutes = require('./routes/propertyRoutes');
 const Property = require('./models/property');
 const fs = require('fs');
+const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
+const hpp = require('hpp');
 //import images
 require('dotenv').config();
+
 const app = express();
 app.use(cors());
-app.use(express.json());
+app.use(helmet());
+app.use(express.json({ limit: '10kb' }));
+app.use(express.urlencoded({ extended: true, limit: '10kb' }));
+app.disable('x-powered-by');
+app.use(hpp());
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+});
+app.use(limiter);
 
 app.use('/api/properties', propertyRoutes);
 //moongodb setup
 const mongoDBUri =
   process.env.MONGODB_URI || 'mongodb://localhost:27017/nexahomes';
+
 mongoose
   .connect(mongoDBUri)
   .then(() => console.log('Connected to MongoDB'))
